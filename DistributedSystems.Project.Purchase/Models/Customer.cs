ec523ad9a19756e5c46524298bcf89481e3006bd
Luis Chavez <luchor13@gmail.com>
2019-07-08 15:58:48 -0500

    first commit

---
+++ DistributedSystems.Project.Purchase/Models/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DistributedSystems.Project.Purchase.Models
+{
+    public partial class Customer
+    {
+        public Customer()
+        {
+            Order = new HashSet<Order>();
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public string FullName {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+
+        public virtual ICollection<Order> Order { get; set; }
+    }
+}
